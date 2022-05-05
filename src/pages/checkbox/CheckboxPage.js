da53d7db35446a661cf6777675468b96daba1ed6
kim-duyen-frontend <ntduyen255@gmail.com>
2022-05-06 01:30:58 +0700

    update checkbox

--- src/pages/checkbox/CheckboxPage.js
+++ src/pages/checkbox/CheckboxPage.js
@@ -1,30 +1,63 @@
 import React, { useState } from "react";
-import { Checkbox } from "semantic-ui-react";
+import { Checkbox, Button } from "semantic-ui-react";
 
 const CheckboxPage = () => {
-  const [checked, setChecked] = useState(false);
-
-  const handleChangeCheckBox = (checked) => {
-    setChecked(checked); //làm dấu tích sáng lên
-    console.log(checked);
-  };
-  return (
-    <div>
-      <Checkbox
-        label="Check this box 1"
-        onChange={(e, data) => handleChangeCheckBox(data.checked)}
-        checked={checked}
-      />
+  const VersionCheckBox_One = () => {
+    const [checked, setChecked] = useState(false);
+    const handleChangeCheckBox = (checked) => {
+      setChecked(checked); //làm dấu tích sáng lên
+      console.log(checked);
+    };
+    return (
       <div>
-        {/* <Button onClick={() => setChecked((prevChecked) => !prevChecked)}>
+        <Checkbox
+          label="Check this box 1"
+          onChange={(e, data) => handleChangeCheckBox(data.checked)}
+          checked={checked}
+        />
+      </div>
+    );
+  };
+  const VersionCheckBox_Two = () => {
+    const [checked, setChecked] = useState(false);
+    return (
+      <div>
+        <Checkbox
+          label="Check this box 1"
+          onChange={(e, data) => setChecked(data.checked)}
+          checked={checked}
+        />
+        <Checkbox
+          label="Check this box 2"
+          onChange={(e, data) => setChecked(data.checked)}
+          checked={checked}
+        />
+      </div>
+    );
+  };
+  const VersionCheckBox_Three = () => {
+    const [checked, setChecked] = useState(false);
+    return (
+      <div>
+        <Button onClick={() => setChecked((prevChecked) => !prevChecked)}>
           Toggle it
         </Button>
         <Checkbox
           label="Check this box 2"
           onChange={(e, data) => setChecked(data.checked)}
           checked={checked}
-        /> */}
+        />
       </div>
+    );
+  };
+  return (
+    <div>
+      <h2>Dạng 1</h2>
+      {VersionCheckBox_One()}
+      <h2>Dạng 2</h2>
+      {VersionCheckBox_Two()}
+      <h2>Dạng 3</h2>
+      {VersionCheckBox_Three()}
     </div>
   );
 };
